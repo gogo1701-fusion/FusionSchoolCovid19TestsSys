028af113746feec05b865e4d69bba7ff8b73b983
Ludiliste <92212390+Retr0A@users.noreply.github.com>
2021-12-15 19:49:43 +0200

    Added little changes + File Writing

--- Covid19 automation test/Form1.Designer.cs
+++ Covid19 automation test/Form1.Designer.cs
@@ -28,70 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(241, 128);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(317, 23);
-            this.textBox1.TabIndex = 0;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(241, 157);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(317, 23);
-            this.textBox2.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(171, 131);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(63, 15);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Username:";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(171, 160);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(60, 15);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Password:";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(296, 186);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(194, 97);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Login";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox1.Location = new System.Drawing.Point(207, 111);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(272, 20);
+			this.textBox1.TabIndex = 0;
+			// 
+			// textBox2
+			// 
+			this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox2.Location = new System.Drawing.Point(207, 136);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.PasswordChar = '*';
+			this.textBox2.Size = new System.Drawing.Size(272, 20);
+			this.textBox2.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(147, 114);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(58, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Username:";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(147, 139);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(56, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Password:";
+			// 
+			// button1
+			// 
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Location = new System.Drawing.Point(254, 185);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(166, 60);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "Login";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label3.Location = new System.Drawing.Point(112, 34);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(464, 31);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Welcome to Covid19 automation test!";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label4.Location = new System.Drawing.Point(-14, 340);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(920, 17);
+			this.label4.TabIndex = 6;
+			this.label4.Text = "=================================================================================" +
+    "=================================\r\n";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+			this.label5.Location = new System.Drawing.Point(-86, 9);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(920, 17);
+			this.label5.TabIndex = 7;
+			this.label5.Text = "=================================================================================" +
+    "=================================\r\n";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(686, 390);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.Text = "Covid19 automation test";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -102,5 +146,8 @@
         private Label label1;
         private Label label2;
         private Button button1;
-    }
+		private Label label3;
+		private Label label4;
+		private Label label5;
+	}
 }
--- Covid19 automation test/Form1.cs
+++ Covid19 automation test/Form1.cs
@@ -1,3 +1,7 @@
+using System.IO;
+using System.Drawing;
+using System.Windows.Forms;
+
 namespace Covid19_automation_test
 {
     public partial class Form1 : Form
@@ -5,6 +9,10 @@ namespace Covid19_automation_test
         public Form1()
         {
             InitializeComponent();
+
+            StreamWriter sw = new StreamWriter(Application.StartupPath + "\\files\\" + "test.txt");
+            sw.WriteLine("This text is written by our app.");
+            sw.Close();
         }
 
         private void label1_Click(object sender, EventArgs e)
