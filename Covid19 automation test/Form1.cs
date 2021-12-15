1b399df8df7c387ee4ee49c09236e4159a557dae
gogo1701-fusion <96187744+gogo1701-fusion@users.noreply.github.com>
2021-12-15 17:37:01 +0200

    files

---
+++ Covid19 automation test/Form1.cs
@@ -0,0 +1,20 @@
+namespace Covid19_automation_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
